342c1ede579a243d61b2c6c97df2222ce969b6ef
andrey20005 <z.andrey2005.10@gmail.com>
2025-03-19 18:31:35 +0300

    сделал норм тест колекцию

--- src/com/itmo/prog_lab5_8/сollection/TestCollectionBuilder.java
+++ src/com/itmo/prog_lab5_8/сollection/TestCollectionBuilder.java
@@ -1,8 +1,6 @@
 package com.itmo.prog_lab5_8.сollection;
 
-import com.itmo.prog_lab5_8.models.Coordinates;
-import com.itmo.prog_lab5_8.models.Dragon;
-import com.itmo.prog_lab5_8.models.DragonCharacter;
+import com.itmo.prog_lab5_8.models.*;
 
 import java.time.ZonedDateTime;
 
@@ -11,47 +9,30 @@ public class TestCollectionBuilder implements CollectionBuilder{
     public Dragons getCollection() {
         Dragons dragons = new Dragons();
 
-        Dragon dragonA = new Dragon();
-//        dragonA.id = (long) 1;
-//        dragonA.name = "dragon_a";
-//        dragonA.coordinates = new Coordinates();
-//        dragonA.coordinates.x = (float) 1.;
-//        dragonA.coordinates.y = (float) 1.;
-//        dragonA.creationDate = ZonedDateTime.now();
-//        dragonA.age = 10;
-//        dragonA.description = "errger";
-//        dragonA.weight = (float) 500;
-//        dragonA.character = DragonCharacter.CHAOTIC;
-//        dragonA.killer = null;
-        dragons.add(dragonA);
+        Dragon dragon = new Dragon();
+        dragon.setId(1L);
+        dragon.setName("Smaug");
+        dragon.setCoordinates(new Coordinates());
+        dragon.getCoordinates().setX(68.648f);
+        dragon.getCoordinates().setY(68.648f);
+        dragon.setCreationDate(ZonedDateTime.now());
+        dragon.setAge(100);
+        dragon.setDescription("A fearsome dragon");
+        dragon.setWeight(500.0f);
+        dragon.setCharacter(DragonCharacter.CUTTING);
 
-        Dragon dragonB = new Dragon();
-//        dragonB.id = (long) 2;
-//        dragonB.name = "dragon_b";
-//        dragonB.coordinates = new Coordinates();
-//        dragonB.coordinates.x = (float) 1.;
-//        dragonB.coordinates.y = (float) 10.;
-//        dragonB.creationDate = ZonedDateTime.now();
-//        dragonB.age = 100;
-//        dragonB.description = "errger";
-//        dragonB.weight = (float) 5000;
-//        dragonB.character = DragonCharacter.CUTTING;
-//        dragonB.killer = null;
-        dragons.add(dragonB);
+        Person killer = new Person();
+        killer.setName("Bard");
+        killer.setHeight(1.85);
+        killer.setEyeColor(Color.BLACK);
+        killer.setHairColor(Color.BROWN);
+        killer.setLocation(new Location());
+        killer.getLocation().setName("Lake-town");
+        killer.getLocation().setX(10);
+        killer.getLocation().setY(20);
 
-        Dragon dragonC = new Dragon();
-//        dragonC.id = (long) 1;
-//        dragonC.name = "dragon_c";
-//        dragonC.coordinates = new Coordinates();
-//        dragonC.coordinates.x = (float) 1.;
-//        dragonC.coordinates.y = (float) -200.;
-//        dragonC.creationDate = ZonedDateTime.now();
-//        dragonC.age = 105;
-//        dragonC.description = "errger";
-//        dragonC.weight = (float) 1000;
-//        dragonC.character = DragonCharacter.FICKLE;
-//        dragonC.killer = null;
-        dragons.add(dragonC);
+        dragon.setKiller(killer);
+        dragons.add(dragon);
 
         return dragons;
     }
