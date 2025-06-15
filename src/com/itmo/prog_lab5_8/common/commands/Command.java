00c16b00d0fe4d36ca91d9296931d252c1e6a550
andrey20005 <z.andrey2005.10@gmail.com>
2025-06-16 02:36:25 +0300

    причесал

--- src/com/itmo/prog_lab5_8/common/commands/Command.java
+++ src/com/itmo/prog_lab5_8/common/commands/Command.java
@@ -8,28 +8,4 @@ import java.nio.ByteBuffer;
 public interface Command extends Serializable {
     void execute(Invoker invoker);
     String getResult();
-
-    static ByteBuffer serializableToByteBuffer(Serializable serializable) throws IOException {
-        try (
-                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-                ObjectOutputStream oos = new ObjectOutputStream(outputStream)
-        ) {
-            oos.writeObject(serializable);
-
-            byte[] array = outputStream.toByteArray();
-
-            ByteBuffer buffer = ByteBuffer.allocate(100000);
-            buffer.put(array);
-            buffer.flip();
-            return buffer;
-        }
-    }
-
-    static Serializable byteBufferToSerializable(ByteBuffer buffer) throws IOException, ClassNotFoundException {
-        byte[] array = new byte[buffer.remaining()];
-        buffer.get(array);
-        try (ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(array))) {
-            return (Serializable) ois.readObject();
-        }
-    }
 }
